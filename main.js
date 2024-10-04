6c37bf71301d3036e446626b0ad1c6661c321ced
naoto64 <51907267+naoto64@users.noreply.github.com>
2024-10-04 22:57:28 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,12 +2,17 @@
 
 function molkkyClick(e){
     if(e.dataset.toggle == "0"){
+        if(Number(document.getElementById("point").textContent) == 0){
+            for(var i = 0; i < 12; i++){
+                document.getElementsByClassName("molkky").item(i).dataset.toggle = "0";
+            }
+        }
         e.dataset.toggle = "1";
-        document.getElementById("point").textContent = Number(document.getElementById("point").textContent) + Number(e.innerText);
+        document.getElementById("point").textContent = Number(e.innerText);
     }
     else if(e.dataset.toggle == "1"){
         e.dataset.toggle = "0";
-        document.getElementById("point").textContent = Number(document.getElementById("point").textContent) - Number(e.innerText);
+        document.getElementById("point").textContent = 0;
     }
 }
 
